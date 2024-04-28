5b2421d0d598cd698f950e02b20127fce7fbf45d
Valerii Sidorov <v.sidorov.photo@gmail.com>
2024-04-28 14:21:05 +0200

    Edit tasks 1, 3, 4, 6

--- lesson_2/lesson_2_task_3.py
+++ lesson_2/lesson_2_task_3.py
@@ -1,9 +1,11 @@
-def square(num):
-    result = num * num
-    return result
+import math
 
 
-storona = float(input("Введите сторону квадрата: "))
+def square(side_length):
+    area = side_length ** 2
+    rounded_area = math.ceil(area)
+    return rounded_area
 
-print("Площадь квадрата со стороной =", storona,
-      ", равна", round(square(storona)))
+
+side = float(input("Введите сторону квадрата: "))
+print("Площадь квадрата с длиной стороны", side, "равна", square(side))
